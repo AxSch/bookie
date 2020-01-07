a8761aed774845e702afe43f7751706fcb69a80a
AxSch <alex_schuneman@outlook.com>
2020-01-07 22:16:52 +0000

    Add endpoint for WishList model

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from books.views import BooksViewSet
+from booksList.views import WishListViewSet
 
 router = DefaultRouter()
 router.register(r'books', BooksViewSet, basename='books')
+router.register(r'wishlist', WishListViewSet, basename='wishlist')
 
 urlpatterns = router.urls
