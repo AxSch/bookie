5d690772194229df9b3b0978c11ab1bb8aa0d7d7
AxSch <alex_schuneman@outlook.com>
2020-01-07 21:19:02 +0000

    Add router & urls for books

---
+++ api/core/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from books.views import BooksViewSet
+
+router = DefaultRouter()
+router.register(r'books', BooksViewSet, basename='books')
+
+urlpatterns = router.urls
