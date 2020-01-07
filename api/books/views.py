a425c3dba5dfcce78ed9df2327a2a6973f09f2f2
AxSch <alex_schuneman@outlook.com>
2020-01-07 21:17:30 +0000

    Create viewset for the books model

--- api/books/views.py
+++ api/books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from .models import Book
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import BookSerializer
 
 # Create your views here.
+
+
+class BooksViewSet(
+    GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
