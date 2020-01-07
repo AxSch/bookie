9b1b1b077c830694d1589ec21f8347896efacb98
AxSch <alex_schuneman@outlook.com>
2020-01-07 22:41:36 +0000

    Update serializer to use nested serialization

--- api/booksList/serializers.py
+++ api/booksList/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import WishList
+from books.serializers import BookSerializer
 
 
 class WishListSerializer(serializers.ModelSerializer):
+    owner = serializers.StringRelatedField()
+    books = BookSerializer(many=True)
+
     class Meta:
         model = WishList
-        fields = ['id', 'name', 'books', 'owner', 'is_public']
+        fields = ["id", "name", "books", "owner", "is_public"]
